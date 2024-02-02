1c76ef3695a7b64816bff553e361a42e642d9807
Zefra37 <zachpieratt@gmail.com>
2024-02-02 00:29:55 -0500

    Added core html for next upcoming movie, basic style sheet and API call/ parse for data. Pass to front end fields

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+
+    const url = 'https://www.whenisthenextmcufilm.com/api'
+
+    const title = document.getElementById('title');
+    const daysUntil = document.getElementById('daysUntil');
+    const releaseDate = document.getElementById('releaseDate');
+    const summary = document.getElementById('summary');
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            title.innerHTML = `${data.title} (${data.type})`;
+            daysUntil.innerHTML = data.days_until;
+            console.log(data.daysUntil);
+            releaseDate.innerHTML = data.release_date;
+            document.getElementById('poster').src = data.poster_url;
+            summary.innerHTML = data.overview;
+        })
+        .catch(error => {
+            console.error('Request Failed', error);
+            title.innerHTML = error;
+        })
 })
