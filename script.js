ea27c5cb6ca6badb6cf675e978f9ba7cc2f0ba05
Zefra37 <zachpieratt@gmail.com>
2024-02-03 00:09:21 -0500

    Initial working version, donate link added and basic CSS. Now shows upcoming next 2 movies

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const releaseDate = document.getElementById('releaseDate');
     const summary = document.getElementById('summary');
 
+    const title2 = document.getElementById('title2');
+    const daysUntil2 = document.getElementById('daysUntil2');
+    const releaseDate2 = document.getElementById('releaseDate2');
+    const summary2 = document.getElementById('summary2');
+
     fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -16,9 +21,28 @@ document.addEventListener('DOMContentLoaded', () => {
             releaseDate.innerHTML = data.release_date;
             document.getElementById('poster').src = data.poster_url;
             summary.innerHTML = data.overview;
+            if (data.following_production?.title) {
+                title2.innerHTML = `${data.following_production.title} (${data.following_production.type})`;
+                daysUntil2.innerHTML = data.following_production.days_until;
+                console.log(data.following_production.daysUntil);
+                releaseDate2.innerHTML = data.following_production.release_date;
+                document.getElementById('poster2').src = data.following_production.poster_url;
+                summary2.innerHTML = data.following_production.overview;
+            }
         })
         .catch(error => {
             console.error('Request Failed', error);
             title.innerHTML = error;
         })
+
+    //const leaveReview = document.getElementById("review");
+    const donate = document.getElementById("donateButton");
+    //const reviewUrl = ""
+    const donateUrl = "https://github.com/sponsors/zefra37?frequency=one-time"
+
+    donate.addEventListener("click", () => {
+        chrome.tabs.create({
+            url: donateUrl
+        });
+    });
 })
