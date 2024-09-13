3ea69a5fdbc241b0d4a181a2f9487da69d7bc96c
zachpieratt <zachpieratt@gmail.com>
2024-09-13 09:11:40 -0400

    Updated github link

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const url = "https://www.whenisthenextmcufilm.com/api";
 
-    const url = 'https://www.whenisthenextmcufilm.com/api'
+  const title = document.getElementById("title");
+  const daysUntil = document.getElementById("daysUntil");
+  const releaseDate = document.getElementById("releaseDate");
+  const summary = document.getElementById("summary");
 
-    const title = document.getElementById('title');
-    const daysUntil = document.getElementById('daysUntil');
-    const releaseDate = document.getElementById('releaseDate');
-    const summary = document.getElementById('summary');
+  const title2 = document.getElementById("title2");
+  const daysUntil2 = document.getElementById("daysUntil2");
+  const releaseDate2 = document.getElementById("releaseDate2");
+  const summary2 = document.getElementById("summary2");
 
-    const title2 = document.getElementById('title2');
-    const daysUntil2 = document.getElementById('daysUntil2');
-    const releaseDate2 = document.getElementById('releaseDate2');
-    const summary2 = document.getElementById('summary2');
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            title.innerHTML = `${data.title} (${data.type})`;
-            daysUntil.innerHTML = data.days_until;
-            console.log(data.daysUntil);
-            releaseDate.innerHTML = data.release_date;
-            document.getElementById('poster').src = data.poster_url;
-            summary.innerHTML = data.overview;
-            if (data.following_production?.title) {
-                title2.innerHTML = `${data.following_production.title} (${data.following_production.type})`;
-                daysUntil2.innerHTML = data.following_production.days_until;
-                console.log(data.following_production.daysUntil);
-                releaseDate2.innerHTML = data.following_production.release_date;
-                document.getElementById('poster2').src = data.following_production.poster_url;
-                summary2.innerHTML = data.following_production.overview;
-            }
-        })
-        .catch(error => {
-            console.error('Request Failed', error);
-            title.innerHTML = error;
-        })
-
-    const leaveReview = document.getElementById("reviewButton");
-    const donate = document.getElementById("donateButton");
-    const reviewUrl = "https://chromewebstore.google.com/detail/upcoming-marvel-releases/igbejccgmkpbbdfijplgobombkdcbmgk?hl=en";
-    const donateUrl = "https://github.com/sponsors/zefra37?frequency=one-time"
-
-    donate.addEventListener("click", () => {
-        chrome.tabs.create({
-            url: donateUrl
-        });
-    });
-    leaveReview.addEventListener("click", () => {
-        chrome.tabs.create({
-            url: reviewUrl
-        });
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      title.innerHTML = `${data.title} (${data.type})`;
+      daysUntil.innerHTML = data.days_until;
+      console.log(data.daysUntil);
+      releaseDate.innerHTML = data.release_date;
+      document.getElementById("poster").src = data.poster_url;
+      summary.innerHTML = data.overview;
+      if (data.following_production?.title) {
+        title2.innerHTML = `${data.following_production.title} (${data.following_production.type})`;
+        daysUntil2.innerHTML = data.following_production.days_until;
+        console.log(data.following_production.daysUntil);
+        releaseDate2.innerHTML = data.following_production.release_date;
+        document.getElementById("poster2").src =
+          data.following_production.poster_url;
+        summary2.innerHTML = data.following_production.overview;
+      }
+    })
+    .catch((error) => {
+      console.error("Request Failed", error);
+      title.innerHTML = error;
     });
 
-})
+  const leaveReview = document.getElementById("reviewButton");
+  const github = document.getElementById("githubButton");
+  const reviewUrl =
+    "https://chromewebstore.google.com/detail/upcoming-marvel-releases/igbejccgmkpbbdfijplgobombkdcbmgk?hl=en";
+  const githubUrl = "https://github.com/zachpieratt";
+
+  github.addEventListener("click", () => {
+    chrome.tabs.create({
+      url: githubUrl,
+    });
+  });
+  leaveReview.addEventListener("click", () => {
+    chrome.tabs.create({
+      url: reviewUrl,
+    });
+  });
+});
